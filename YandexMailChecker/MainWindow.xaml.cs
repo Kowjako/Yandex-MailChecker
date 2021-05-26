c49bab09184b02b813ff4d3435ec82d8569ffbd5
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-05-26 23:04:45 +0200

    Add functionality for header buttons

--- YandexMailChecker/MainWindow.xaml.cs
+++ YandexMailChecker/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace YandexMailChecker
 {
@@ -20,9 +11,28 @@ namespace YandexMailChecker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string path = @"D:\YandexMailChecker\";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void closeBtn_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Thread.Sleep(100);
+            this.Close();
+        }
+
+        private void catalogueBtn_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            DirectoryInfo outputDirectory = new DirectoryInfo(path);
+            if (!outputDirectory.Exists) outputDirectory.Create();
+        }
+
+        private void headerPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
         }
     }
 }
