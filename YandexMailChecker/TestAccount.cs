b0289fe53b2dcfc646d71164b7ba2fca8e8a30ec
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-06-02 14:20:56 +0200

    Completed Account class

---
+++ YandexMailChecker/TestAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YandexMailChecker
+{
+    class TestAccount : INotifyPropertyChanged
+    {
+        private string login;
+        private string password;
+
+        public string Login
+        {
+            get { return login; }
+            set
+            {
+                login = value;
+                OnPropertyChanged("Login");
+            }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
