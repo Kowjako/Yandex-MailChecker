9ea7038bc4734c86c4093f70bb02e427d527b7f0
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-06-03 18:34:10 +0200

    Worked browser navigation by Uri

--- YandexMailChecker/ContactWindow.xaml.cs
+++ YandexMailChecker/ContactWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,9 +28,10 @@ namespace YandexMailChecker
             InitializeComponent();
         }
 
-        public ContactWindow(string type, int x) : this()    //wywolanie domyslnego konstruktora aby zainicjalizowac View, bo ten jest przeciazony
+        public ContactWindow(string type) : this()    //wywolanie domyslnego konstruktora aby zainicjalizowac View, bo ten jest przeciazony
         {
             this.type = type;
+            Hyperlink hyperLink = new Hyperlink();
             switch(type)
             {
                 case "gmail":
@@ -40,19 +42,37 @@ namespace YandexMailChecker
                 case "instagram":
                     infoLabel.Text = defaultString + "Instagram";
                     contactData.Inlines.Clear();
-                    contactData.Inlines.Add(new Hyperlink() { NavigateUri = new Uri("https://instagram.com/wlodzimierzyk") });
+                    hyperLink.Inlines.Clear();
+                    hyperLink.NavigateUri = new Uri("https://instagram.com/wlodzimierzyk");
+                    hyperLink.Inlines.Add("https://instagram.com/wlodzimierzyk");
+                    hyperLink.RequestNavigate += HyperLink_RequestNavigate;
+                    contactData.Inlines.Add(hyperLink);
                     break;
                 case "vk":
                     infoLabel.Text = defaultString + "VK";
                     contactData.Inlines.Clear();
-                    contactData.Inlines.Add(new Hyperlink() { NavigateUri = new Uri("https://vk.com/kovyako") });
+                    hyperLink.Inlines.Clear();
+                    hyperLink.NavigateUri = new Uri("https://vk.com/kovyako");
+                    hyperLink.Inlines.Add("https://vk.com/kovyako");
+                    hyperLink.RequestNavigate += HyperLink_RequestNavigate;
+                    contactData.Inlines.Add(hyperLink);
                     break;
-                case "faceboook":
+                case "facebook":
                     infoLabel.Text = defaultString + "Facebook";
                     contactData.Inlines.Clear();
-                    contactData.Inlines.Add(new Hyperlink() { NavigateUri = new Uri("https://www.facebook.com/kowyako") });
+                    hyperLink.Inlines.Clear();
+                    hyperLink.NavigateUri = new Uri("https://www.facebook.com/kowyako");
+                    hyperLink.Inlines.Add("https://www.facebook.com/kowyako");
+                    hyperLink.RequestNavigate += HyperLink_RequestNavigate;
+                    contactData.Inlines.Add(hyperLink);
                     break;
             }
+        }
+
+        private void HyperLink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(((Hyperlink)sender).NavigateUri.ToString()));
+            e.Handled = true;
         }
 
         private void closeBtn_MouseUp(object sender, MouseButtonEventArgs e)
